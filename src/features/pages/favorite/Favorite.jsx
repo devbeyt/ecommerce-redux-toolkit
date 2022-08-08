bd3e9ddd342b1868f16c9ea2b97438d611971e5b
= <=>
2022-08-08 23:02:32 +0400

    updated favorites page

--- src/features/pages/favorite/Favorite.jsx
+++ src/features/pages/favorite/Favorite.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import './../../styles/styles.css'
 import { favoritesSelector, removeFromFavorites } from './favoritesSlice'
 
 function Favorite() {
     const favorites = useSelector(favoritesSelector)
     const dispatch = useDispatch()
+    const navigate = useNavigate()
    
     const removeFavory = (favory)=>{
          dispatch(removeFromFavorites(favory))
     }
+
+    const getProductDetails = (favory)=>{
+      navigate(`/products/${favory.id}`)
+      }
 
     const content = favorites.map(favory=>{
       const {image,title,id,description,price} = favory
       return <div key={id} className="favorite_product">
          <span className='favory_title'>{title}</span>
           <div className="favorite_img_wrapper">
-          <img src={image} className="favory_img" alt={title}/>
+          <img src={image} className="favory_img" alt={title} onClick={()=>getProductDetails(favory)}/>
           </div>
           <span className='favorite_description'>{description.substring(1,80)}</span>
           <button type='button' className='remove_btn' onClick={()=>removeFavory(favory)}>remove</button>
