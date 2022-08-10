7c0b3f570e5ea1ec68830ec4097fc7e929e448bf
= <=>
2022-08-10 20:41:44 +0400

    8 10 2022

--- src/features/pages/cart/CartItem.jsx
+++ src/features/pages/cart/CartItem.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './../../styles/styles.css'
 import { removeFromCart } from './cartSlice';
 
 function CartItem({ item }) {
     // const { id, title, price,image } = item;
+    const navigate = useNavigate();
     const dispatch = useDispatch()
 
     const handleRemove =(item)=>{
         dispatch(removeFromCart(item))
     }
 
+    const getProductDetails = (product)=>{
+        navigate(`/products/${product.id}`)
+  }
+
     return (
         <div className='cart_item' key={item.id}>
                 
                 <div className="item_view">
                 <h5 className='item_title'>{item.title}</h5>
-                <img src={item.image} alt={item.title} className="item_img" />
+                <img src={item.image} alt={item.title} className="item_img" onClick={()=>getProductDetails(item)}/>
                 </div>
 
                 <span className='item_price'>Price: {item.price} $</span>
